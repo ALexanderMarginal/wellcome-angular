86628250a3c1fc9ea9d66f29302df465cb3bfc5c
18270354 <gytraskovskiy@sberbank.ru>
2020-12-30 09:44:46 +0300

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { MatButtonModule, MatCardModule, MatGridListModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatGridListModule, MatToolbarModule } from '@angular/material';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService} from '@/app/services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [
     DishService,
