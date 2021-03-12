d7639c2ef2b167ab100dfe3a57031127a4d7be3e
Geniy <gytraskovskiy@sberbank.ru>
2021-03-12 13:14:51 +0300

    rework dishdetail using angular router

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,7 @@
+import { DishService } from '@/app/services/dish.service';
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from 'src/app/shared/dish';
 
 @Component({
@@ -7,12 +10,20 @@ import { Dish } from 'src/app/shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  @Input()
-  dish: Dish|null = null;
+  dish: Dish;
 
-  constructor() { }
+  constructor(
+    private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
